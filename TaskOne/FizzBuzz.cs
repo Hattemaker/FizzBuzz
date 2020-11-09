bfd84cd56142f187ff5cc926a7533fd3229a182f
Magnus Neergaard <mb.neergaard@gmail.com>
2020-11-09 12:37:16 +0100

    endret mappestruktur

---
+++ TaskOne/FizzBuzz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public static void Main()
+        {
+            RunGame(1, 100);
+        }
+
+        static void RunGame(int min, int max)
+        {
+            for (var i = min; i <= max; i++)
+            {
+                //set the multiple rules for this program
+                var x = (i % 3 == 0) && (i % 5 == 0);
+                var y = i % 3 == 0;
+                var z = i % 5 == 0;
+
+                //checks and replaces if conditions are met
+                if (x) Console.WriteLine("FizzBuzz");
+                else if (y) Console.WriteLine("Fizz");
+                else if (z) Console.WriteLine("Buzz");
+                else Console.WriteLine(i);
+            }
+        }
+    }
+}
